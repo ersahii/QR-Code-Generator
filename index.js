222a3cc318bc386fab6f3f9564a461c934bf545a
mirmehraj <mehraj95he@gmail.com>
2024-06-06 15:33:04 +0530

    Created QR code generator using enquirer and qr-image modules

--- index.js
+++ index.js
@@ -16,7 +16,35 @@
 // import generateName from "sillyname"
 // var sillyname = generateName();
 // console.log(sillyname);
-import {randomSuperhero} from 'superheroes';
+// import {randomSuperhero} from 'superheroes';
 
 
-console.log(`My superhero name is ${randomSuperhero()}`);
+// console.log(`My superhero name is ${randomSuperhero()}`);
+//  Generate Qr code  using inquirer npm 
+// 1. use the inquirer npm package to get user input.
+import inquirer from 'inquirer';
+import qr from "qr-image";
+import fs from "fs";
+// var qr = require('qr-image');
+inquirer
+  .prompt([
+    {message: "Type in your URL",
+        name : "URL"
+    }
+  ])
+  .then((answers) => {
+    const url = answers.URL;
+    var qr_svg = qr.image(url);
+    qr_svg.pipe(fs.createWriteStream('qr_img1.png'));
+    fs.writeFile("URL.txt", url, (err)=>{
+        if(err) throw err;
+        console.log("File saved Successfully");
+    })
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
